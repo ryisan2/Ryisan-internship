437e9e6ef4d9dca0a6707e5a21b5849ad359bb61
rysian2 <ryisan2@gmail.com>
2024-08-17 22:38:42 -0400

    morework

--- src/components/allitems.jsx
+++ src/components/allitems.jsx
@@ -4,8 +4,8 @@ import Countdown from "./UI/Countdown";
 
 const AllItems = ({ item }) => {
   return (
-    <div className="nft__item mb-5 h-full flex flex-col justify-between border border-gray-200 rounded-lg overflow-hidden" data-aos="fade-in">
-      <div className="flex items-start p-4">
+    <div className="nft__item h-full" data-aos="fade-in">
+      <div className="flex items-start author_list_pp">
         <Link
           to={`/author/${item.authorId}`}
           data-bs-toggle="tooltip"
@@ -33,20 +33,20 @@ const AllItems = ({ item }) => {
         <Link to={`/item-details/${item.nftId}`}>
           <img
             src={item.nftImage}
-            className="lazy h-48 w-full object-cover"
+            className="lazy h-64 w-full object-cover"
             alt=""
           />
         </Link>
       </div>
 
-      <div className="nft__item_info min-h-[100px] mt-4 p-4">
+      <div className="nft__item_info min-h-[100px] mt-4">
         <Link to={`/item-details/${item.nftId}`}>
           <h4 className="text-center">{item.title || "Untitled"}</h4>
         </Link>
-        <div className="nft__item_price text-center mt-2">
+        <div className="nft__item_price text-center">
           {item.price} ETH
         </div>
-        <div className="nft__item_like text-center mt-2">
+        <div className="nft__item_like text-center">
           <i className="fa fa-heart"></i>
           <span>{item.likes}</span>
         </div>
--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -1,77 +1,97 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Skeleton from "../UI/Skeleton";
+import AllItems from "../allitems";
 
 const ExploreItems = () => {
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [itemDisplay, setItemDisplay] = useState(8);
+
+  async function fetchItems(filter) {
+    setLoading(true);
+    try {
+      const response = await axios.get(
+        filter
+          ? `https://us-central1-nft-cloud-functions.cloudfunctions.net/explore?filter=${filter}`
+          : `https://us-central1-nft-cloud-functions.cloudfunctions.net/explore`
+      );
+      setItems(response.data);
+    } catch (error) {
+      console.error("Error fetching items:", error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchItems();
+  }, []);
+
+  function skeleton(index) {
+    return (
+      <div
+        key={index}
+        className="col-lg-3 col-md-6 col-sm-6 col-xs-12 pb-4"
+        style={{ display: "block", backgroundSize: "cover" }}
+      >
+        <Skeleton width="100%" height={440} borderRadius={15} />
+      </div>
+    );
+  }
+
+  function loadMore() {
+    setItemDisplay((prevDisplay) => prevDisplay + 4);
+  }
+
+  function loadLess() {
+    setItemDisplay((prevDisplay) => Math.max(prevDisplay - 8,));
+  }
+
   return (
     <>
       <div>
-        <select id="filter-items" defaultValue="">
+        <select
+          id="filter-items"
+          defaultValue=""
+          onChange={(event) => fetchItems(event.target.value)}
+        >
           <option value="">Default</option>
           <option value="price_low_to_high">Price, Low to High</option>
           <option value="price_high_to_low">Price, High to Low</option>
           <option value="likes_high_to_low">Most liked</option>
         </select>
       </div>
-      {new Array(8).fill(0).map((_, index) => (
-        <div
-          key={index}
-          className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
-          style={{ display: "block", backgroundSize: "cover" }}
-        >
-          <div className="nft__item">
-            <div className="author_list_pp">
-              <Link
-                to="/author"
-                data-bs-toggle="tooltip"
-                data-bs-placement="top"
-              >
-                <img className="lazy" src={AuthorImage} alt="" />
-                <i className="fa fa-check"></i>
-              </Link>
-            </div>
-            <div className="de_countdown">5h 30m 32s</div>
 
-            <div className="nft__item_wrap">
-              <div className="nft__item_extra">
-                <div className="nft__item_buttons">
-                  <button>Buy Now</button>
-                  <div className="nft__item_share">
-                    <h4>Share</h4>
-                    <a href="" target="_blank" rel="noreferrer">
-                      <i className="fa fa-facebook fa-lg"></i>
-                    </a>
-                    <a href="" target="_blank" rel="noreferrer">
-                      <i className="fa fa-twitter fa-lg"></i>
-                    </a>
-                    <a href="">
-                      <i className="fa fa-envelope fa-lg"></i>
-                    </a>
-                  </div>
-                </div>
-              </div>
-              <Link to="/item-details">
-                <img src={nftImage} className="lazy nft__item_preview" alt="" />
-              </Link>
+      {loading ? (
+        <>
+          {new Array(8).fill(0).map((_, index) => skeleton(index))}
+        </>
+      ) : (
+        <div className="row">
+          {items.slice(0, itemDisplay).map((item, index) => (
+            <div
+              key={index}
+              className="col-lg-3  col-sm-6 col-xs-12 pb-4"
+              
+            >
+              <AllItems item={item} />
             </div>
-            <div className="nft__item_info">
-              <Link to="/item-details">
-                <h4>Pinky Ocean</h4>
-              </Link>
-              <div className="nft__item_price">1.74 ETH</div>
-              <div className="nft__item_like">
-                <i className="fa fa-heart"></i>
-                <span>69</span>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
-      ))}
-      <div className="col-md-12 text-center">
-        <Link to="" id="loadmore" className="btn-main lead">
-          Load more
-        </Link>
+      )}
+
+      <div className="col-md-12 text-center mt-3">
+        {itemDisplay > 8 && (
+          <button onClick={loadLess} id="loadless" className="btn-main lead mr-2">
+            Load Less
+          </button>
+        )}
+        {itemDisplay < items.length && (
+          <button onClick={loadMore} id="loadmore" className="btn-main lead">
+            Load More
+          </button>
+        )}
       </div>
     </>
   );
