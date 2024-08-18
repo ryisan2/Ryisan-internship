93c7c8a949b8c6df1cf3c2ac4d440e8d378c405d
rysian2 <ryisan2@gmail.com>
2024-08-18 15:15:04 -0400

    new work

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -1,24 +1,26 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Skeleton from "../UI/Skeleton";
+import "aos/dist/aos.css";
+import Aos from "aos";
 import AllItems from "../allitems";
 
-const ExploreItems = () => {
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+const NewItems = () => {
   const [items, setItems] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [itemDisplay, setItemDisplay] = useState(8);
+  const [centerSlidePercentage, setCenterSlidePercentage] = useState(33.33);
 
-  async function fetchItems(filter) {
-    setLoading(true);
+  async function fetchItems() {
     try {
       const response = await axios.get(
-        filter
-          ? `https://us-central1-nft-cloud-functions.cloudfunctions.net/explore?filter=${filter}`
-          : `https://us-central1-nft-cloud-functions.cloudfunctions.net/explore`
+        `https://us-central1-nft-cloud-functions.cloudfunctions.net/newItems`
       );
       setItems(response.data);
     } catch (error) {
-      console.error("Error fetching items:", error);
+      console.error("Failed to fetch items", error);
     } finally {
       setLoading(false);
     }
@@ -26,75 +28,77 @@ const ExploreItems = () => {
 
   useEffect(() => {
     fetchItems();
+    Aos.init();
+
+    const handleResize = () => {
+      if (window.innerWidth < 768) {
+        setCenterSlidePercentage(100);
+      } else {
+        setCenterSlidePercentage(33.33);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+    handleResize(); // Call it initially to set the correct value
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
   }, []);
 
-  function skeleton(index) {
-    return (
-      <div
-        key={index}
-        className="col-lg-3 col-md-6 col-sm-6 col-xs-12 pb-4"
-        style={{ display: "block", backgroundSize: "cover" }}
-      >
-        <Skeleton width="100%" height={440} borderRadius={15} />
-      </div>
-    );
-  }
-
-  function loadMore() {
-    setItemDisplay((prevDisplay) => prevDisplay + 4);
-  }
-
-  function loadLess() {
-    setItemDisplay((prevDisplay) => Math.max(prevDisplay - 8,));
-
-  }
-
   return (
-    <>
-      <div>
-        <select
-          id="filter-items"
-          defaultValue=""
-          onChange={(event) => fetchItems(event.target.value)}
-        >
-          <option value="">Default</option>
-          <option value="price_low_to_high">Price, Low to High</option>
-          <option value="price_high_to_low">Price, High to Low</option>
-          <option value="likes_high_to_low">Most liked</option>
-        </select>
-      </div>
-
-      {loading ? (
-        <>
-          {new Array(8).fill(0).map((_, index) => skeleton(index))}
-        </>
-      ) : (
+    <section id="section-collections" className="">
+      <div className="container">
         <div className="row">
-          {items.slice(0, itemDisplay).map((item, index) => (
-            <div
-              key={index}
-              className="col-lg-3  col-sm-6 col-xs-12 pb-4"
-            >
-              <AllItems item={item} />
-            </div>
-          ))}
+          <div className="text-center">
+            <h2 data-aos="fade-in">New Items</h2>
+            <div className=" bg-color-2"></div>
+          </div>
         </div>
-      )}
+        <div className="row justify-center align-center">
+          <div>
+            <Carousel
+              showArrows={true}
+              showThumbs={false}
+              infiniteLoop={true}
+              centerMode={true}
+              centerSlidePercentage={centerSlidePercentage}
+              showStatus={false}
+            >
+              {loading
+                ? Array.from({ length: 4 }).map((_, index) => (
+                    <div className="p-2" key={index}>
+                      <div className="nft_coll">
+                        <div className="nft_wrap">
+                          <Skeleton width={500} height={270} />
+                        </div>
+                        <div className="nft_coll_pp">
+                          <Skeleton width={50} height={50} borderRadius={99} />
+                          <i className="fa fa-check"></i>
+                        </div>
+                        <div className="nft_coll_info">
+                          <h4>
+                            <Skeleton height={20} width="40%" />
+                          </h4>
+                          <span>
+                            <Skeleton height={20} width="20%" />
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                  ))
+                : items.map((item, index) => (
+                  <div className="p-2" key={index}>
+                  <AllItems item={item} loading={loading} />
+                </div>
+              ))}
 
-      <div className="col-md-12 text-center mt-3">
-        {itemDisplay > 8 && (
-          <button onClick={loadLess} id="loadless" className="btn-main lead mr-2">
-            Load Less
-          </button>
-        )}
-        {itemDisplay < items.length && (
-          <button onClick={loadMore} id="loadmore" className="btn-main lead">
-            Load More
-          </button>
-        )}
+            </Carousel>
+          </div>
+        </div>
       </div>
-    </>
+    </section>
   );
 };
 
-export default ExploreItems;
+export default NewItems;
